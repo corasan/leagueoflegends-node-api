6985e4aa777d931c953d021d62d376db562dca2e
Henry Paulino <henrypl360@gmail.com>
2016-10-24 19:17:06 -0400

    change to OOP JavaScript

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
 const request = require('request-promise');
-const api_key = process.env.LOL_API_KEY;
 
 // https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{summoner}?{api_key}
-const Lol = {
-  findSummoner: (summonerName, region) => {
+const Lol = function(api_key, region) {
+  this.findSummoner = (summonerName) => {
     request({
       uri: `https://${region}.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${summonerName}?api_key=${api_key}`,
       json: true
@@ -14,5 +13,7 @@ const Lol = {
   }
 }
 
+const api_key = process.env.LOL_API_KEY;
+lol1 = new Lol(api_key, 'na');
 
-Lol.findSummoner('shp corasan', 'na');
+lol1.findSummoner('shp corasan');
