6842a0c63b57258a444c447dd2f8791255b2b65e
Henry Paulino <henrypl360@gmail.com>
2016-10-25 14:00:31 -0400

    fix error including a non-existent file, put URL of summoner api in variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const api_key = process.env.LOL_API_KEY;
-const LeagueofLegends = require('./dummy');
+const LeagueofLegends = require('./src/main');
 require('./src/Summoner');
 
-module.exports = LeagueofLegends;
+const lol = new LeagueofLegends(api_key, 'na');
+lol.findSummoner('shp corasan');
+// module.exports = LeagueofLegends;
--- src/Summoner.js
+++ src/Summoner.js
@@ -1,10 +1,15 @@
 const request = require('request-promise');
 const Lol = require('./main');
+const summonerURL = '.api.pvp.net/api/lol/na/v1.4/summoner';
 
 Lol.prototype.findSummoner = function(summonerName) {
   request({
-    uri: `https://${this.region}.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${summonerName}?api_key=${this.api_key}`,
+    uri: `https://${this.region}${summonerURL}/by-name/${summonerName}?api_key=${this.api_key}`,
     json: true
   })
   .then((response) => console.log(response));
 }
+//
+// Lol.prototype.summonerById = function(summonerId) {
+//
+// }
