5431a77f18205d21bfaef348c622615ed09c1032
Henry Paulino <henrypl360@gmail.com>
2016-10-25 14:20:41 -0400

    add mocha for testing, move dotenv to dev dependencies

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const api_key = process.env.LOL_API_KEY;
 const LeagueofLegends = require('./src/main');
 require('./src/Summoner');
 
-const lol = new LeagueofLegends(api_key, 'na');
-lol.findSummoner('shp corasan');
-// module.exports = LeagueofLegends;
+module.exports = LeagueofLegends;
