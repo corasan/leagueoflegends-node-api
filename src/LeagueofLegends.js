1223841a10f06d75d5844b2b0c59534f73bc002c
Henry Paulino <henrypl360@gmail.com>
2016-12-08 03:04:35 -0500

    using es6, classes in the LeagueofLegends file

--- src/LeagueofLegends.js
+++ src/LeagueofLegends.js
@@ -1,6 +1,10 @@
-const LeagueofLegends = function(api_key, region) {
-  this.api_key = api_key;
-  this.region = region;
-};
+'use strict';
+
+class LeagueofLegends {
+  constructor(api_key, region) {
+    this.api_key = api_key;
+    this.region = region;
+  }
+}
 
 module.exports = LeagueofLegends;
